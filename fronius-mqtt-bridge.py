013ce96f98b8ac14a40cca249b37ee0d58c1bd54
Andreas Kleber <andreas@drosselweg7a.de>
2018-03-28 18:52:17 +0200

    simplification

--- fronius-mqtt-bridge.py
+++ fronius-mqtt-bridge.py
@@ -11,74 +11,58 @@ BROKER_HOST = 'raspberrypi.fritz.box'
 BROKER_PORT = 1883
 FREQUENCY = 2
 
-
 def fronius_data():
-    p_pv = -0.1
-    p_grid = -0.1
-    p_akku = -0.1
-    p_load = -0.1
-    soc = 0
+
+    values = {}
 
     try:
         url = "http://{}/solar_api/v1/GetPowerFlowRealtimeData.fcgi".format(FRONIUS_HOST)  # noqa E501
         r = requests.get(url, timeout=FREQUENCY - 0.5)
         r.raise_for_status()
         powerflow_data = r.json()
-        p_pv = powerflow_data['Body']['Data']['Site']['P_PV']
-        p_grid = powerflow_data['Body']['Data']['Site']['P_Grid']
-        p_akku = powerflow_data['Body']['Data']['Site']['P_Akku']
-        p_load = -powerflow_data['Body']['Data']['Site']['P_Load']
-        soc = powerflow_data['Body']['Data']['Inverters']['1']['SOC']
+        
+        values['p_pv'] = powerflow_data['Body']['Data']['Site']['P_PV']
+        values['p_grid'] = powerflow_data['Body']['Data']['Site']['P_Grid']
+        values['p_akku'] = powerflow_data['Body']['Data']['Site']['P_Akku']
+        values['p_load'] = -powerflow_data['Body']['Data']['Site']['P_Load']
+        values['soc'] = powerflow_data['Body']['Data']['Inverters']['1']['SOC']
+
     except requests.exceptions.Timeout:
         print("Timeout requesting {}".format(url))
     except requests.exceptions.RequestException as e:
         print("requests exception {}".format(e))
 
-    return (p_pv, p_grid, p_akku, p_load, soc)
+    return values
 
 
 if __name__ == '__main__':
     logging.basicConfig(stream=sys.stderr, level=logging.INFO)
     logging.getLogger("urllib3").setLevel(logging.INFO)
 
-    mqttc = paho.Client('fronius-mqtt-brifge', clean_session=True)
+    mqttc = paho.Client('fronius-mqtt-bridge', clean_session=True)
     # mqttc.enable_logger()
-    mqttc.will_set("fronius/bridgestatus", "LOST_CONNECTION", 0, False)
+    mqttc.will_set("fronius/bridgestatus", "LOST_CONNECTION", 0, retain=True)
 
     mqttc.connect(BROKER_HOST, BROKER_PORT, 60)
     logging.info("Connected to {}:{}".format(BROKER_HOST, BROKER_PORT))
 
-    mqttc.publish("fronius/bridgestatus", "ON-LINE")
+    mqttc.publish("fronius/bridgestatus", "ON-LINE", retain=True)
 
     mqttc.loop_start()
     while True:
         try:
-            (p_pv, p_grid, p_akku, p_load, soc) = fronius_data()
-            logging.debug("Data from fronius: p_pv {}, p_grid {}, p_akku {}, p_load {}".format(p_pv, p_grid, p_akku, p_load))  # noqa E501
-            (result, mid) = mqttc.publish('fronius/p_pv', str(p_pv), 0)
-            logging.debug("Pubish Result: {} MID: {} p_pv: {}".format(result, mid, p_pv))  # noqa E501
-            (result, mid) = mqttc.publish('fronius/p_grid', str(p_grid), 0)
-            logging.debug("Pubish Result: {} MID: {} p_grid: {}".format(result, mid, p_grid))  # noqa E501
-            (result, mid) = mqttc.publish('fronius/p_akku', str(p_akku), 0)
-            logging.debug("Pubish Result: {} MID: {} p_akku: {}".format(result, mid, p_akku))  # noqa E501
-            (result, mid) = mqttc.publish('fronius/p_load', str(p_load), 0)
-            logging.debug("Pubish Result: {} MID: {} p_load: {}".format(result, mid, p_load))  # noqa E501
-            (result, mid) = mqttc.publish('fronius/soc', str(soc), 0)
-            logging.debug("Pubish Result: {} MID: {} soc: {}".format(result, mid, soc))  # noqa E501
+            values = fronius_data()
+            for k, v in values.items():
+                (result, mid) = mqttc.publish("fronius/{}".format(k), str(v), 0)
+                logging.debug("Pubish Result: {} MID: {} for {}: {}".format(result, mid, k, v))  # noqa E501                
+
             time.sleep(FREQUENCY)
         except KeyboardInterrupt:
             break
         except Exception:
             raise
 
-    # Set values to 0 and retain this value
-    mqttc.publish('fronius/p_pv', str(0), 0, retain=True)
-    mqttc.publish('fronius/p_grid', str(0), 0, retain=True)
-    mqttc.publish('fronius/p_akku', str(0), 0, retain=True)
-    mqttc.publish('fronius/p_load', str(0), 0, retain=True)
-    mqttc.publish('fronius/soc', str(0), 0, retain=True)
-
-    mqttc.publish("fronius/bridgestatus", "OFF-LINE")
+    mqttc.publish("fronius/bridgestatus", "OFF-LINE", retain=True)
 
     mqttc.disconnect()
     mqttc.loop_stop()  # waits, until DISCONNECT message is sent out
