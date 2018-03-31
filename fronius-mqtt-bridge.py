de2f3c66d32f77236da4045c98dfffb7c385b446
Andreas Kleber <andreas@drosselweg7a.de>
2018-03-31 17:10:05 +0200

    adds mqtt prefix option, batterymode, e_day values

--- fronius-mqtt-bridge.py
+++ fronius-mqtt-bridge.py
@@ -7,6 +7,7 @@ import logging
 import sys
 
 FRONIUS_HOST = 'fronius.fritz.box'
+FRONIUS_MQTT_PREFIX = 'fronius'
 BROKER_HOST = 'raspberrypi.fritz.box'
 BROKER_PORT = 1883
 FREQUENCY = 2
@@ -26,6 +27,9 @@ def fronius_data():
         values['p_akku'] = powerflow_data['Body']['Data']['Site']['P_Akku']
         values['p_load'] = -powerflow_data['Body']['Data']['Site']['P_Load']
         values['soc'] = powerflow_data['Body']['Data']['Inverters']['1']['SOC']
+        values['battery_mode'] = powerflow_data['Body']['Data']['Inverters']['1']['Battery_Mode']
+        values['e_day'] = powerflow_data['Body']['Data']['Inverters']['1']['E_Day']
+
 
     except requests.exceptions.Timeout:
         print("Timeout requesting {}".format(url))
@@ -41,19 +45,19 @@ if __name__ == '__main__':
 
     mqttc = paho.Client('fronius-mqtt-bridge', clean_session=True)
     # mqttc.enable_logger()
-    mqttc.will_set("fronius/bridgestatus", "LOST_CONNECTION", 0, retain=True)
+    mqttc.will_set("{}/bridgestatus".format(FRONIUS_MQTT_PREFIX), "LOST_CONNECTION", 0, retain=True)
 
     mqttc.connect(BROKER_HOST, BROKER_PORT, 60)
     logging.info("Connected to {}:{}".format(BROKER_HOST, BROKER_PORT))
 
-    mqttc.publish("fronius/bridgestatus", "ON-LINE", retain=True)
+    mqttc.publish("{}/bridgestatus".format(FRONIUS_MQTT_PREFIX), "ON-LINE", retain=True)
 
     mqttc.loop_start()
     while True:
         try:
             values = fronius_data()
             for k, v in values.items():
-                (result, mid) = mqttc.publish("fronius/{}".format(k), str(v), 0)
+                (result, mid) = mqttc.publish("{}/{}".format(FRONIUS_MQTT_PREFIX, k), str(v), 0)
                 logging.debug("Pubish Result: {} MID: {} for {}: {}".format(result, mid, k, v))  # noqa E501                
 
             time.sleep(FREQUENCY)
@@ -62,7 +66,7 @@ if __name__ == '__main__':
         except Exception:
             raise
 
-    mqttc.publish("fronius/bridgestatus", "OFF-LINE", retain=True)
+    mqttc.publish("{}/bridgestatus".format(FRONIUS_MQTT_PREFIX), "OFF-LINE", retain=True)
 
     mqttc.disconnect()
     mqttc.loop_stop()  # waits, until DISCONNECT message is sent out
