d6dd5be19549b3850882631a41cc41261aedf9b2
Andreas Kleber <andreas@drosselweg7a.de>
2019-05-08 09:07:02 +0200

    zoe connector working

--- zoe-connector.py
+++ zoe-connector.py
@@ -5,6 +5,8 @@ import time
 import logging
 import sys
 
+from zoe_api.python.shared.zeservices import ZEServices
+
 import secrets
 
 ZOE_MQTT_PREFIX = 'zoe'
@@ -13,27 +15,27 @@ BROKER_PORT = 1883
 FREQUENCY = 600  # sec
 
 
-def login():
-    pass
-
-
-def refresh():
-    pass
-
-
-def getToken():
-    print(secrets.VIN)
-    # login or refresh
-
-
 def getSocRange():
-    pass
+    zeServices = ZEServices(secrets.ZE_USERNAME, secrets.ZE_PASSWORD)
+
+    # ZE Services vehicle status.
+    zeServices_json = zeServices.apiCall('/api/vehicle/' + secrets.VIN + '/battery')
+
+    soc = zeServices_json['charge_level']
+    remaining_range = zeServices_json['remaining_range']
+    logging.info("Zoe API: soc: {}%, range: {}km".format(soc, remaining_range))
+
+    return soc, remaining_range
 
 
 def update():
-    pass
-    # (result, mid) = mqttc.publish("{}/{}".format(BATTERY_MQTT_PREFIX, k), str(v), 0, retain=True) # noqa E501
-    # logging.info("Pubish Result: {} MID: {} for {}: {}".format(result, mid, k, v)) # noqa E501
+    soc, remaining_range = getSocRange()
+
+    (result, mid) = mqttc.publish("{}/{}".format(ZOE_MQTT_PREFIX, 'soc'), str(soc), 0, retain=True)
+    logging.info("Pubish Result: {} MID: {} for {}: {}".format(result, mid, 'soc', soc))
+
+    (result, mid) = mqttc.publish("{}/{}".format(ZOE_MQTT_PREFIX, 'range'), str(remaining_range), 0, retain=True)
+    logging.info("Pubish Result: {} MID: {} for {}: {}".format(result, mid, 'range', remaining_range))
 
 
 if __name__ == '__main__':
