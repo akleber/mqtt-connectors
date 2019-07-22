28d09987b57adef8f29a0bd9699c63ab4b2a027c
Andreas Kleber <andreas@drosselweg7a.de>
2019-07-22 16:17:30 +0200

    netatmo connector WIP

---
+++ netatmo-connector.py
@@ -0,0 +1,74 @@
+#!/usr/bin/env python
+
+import paho.mqtt.client as paho  # pip install paho-mqtt
+import pyatmo
+import time
+import logging
+import sys
+
+NETATMO_MQTT_PREFIX = 'netatmo'
+BROKER_HOST = 'rpi3.fritz.box'
+BROKER_PORT = 1883
+FREQUENCY = 10 * 60  # in seconds
+
+CLIENT_ID = ''
+CLIENT_SECRET = ''
+USERNAME = ''
+PASSWORD = ''
+
+
+def get_values():
+
+    values = {}
+
+    try:
+        authorization = pyatmo.ClientAuth(
+            clientId=CLIENT_ID,
+            clientSecret=CLIENT_SECRET,
+            username=USERNAME,
+            password=PASSWORD,
+        )
+
+        weatherData = pyatmo.WeatherStationData(authorization)
+
+        values['outdoor/temp'] = weatherData.lastData()["Outdoor"]["Temperature"]
+        values['indoor/temp'] = weatherData.lastData()["Wohnzimmer"]["Temperature"]
+
+    except Exception as e:
+        print("exception {}".format(e))
+
+    return values
+
+
+if __name__ == '__main__':
+    logging.basicConfig(stream=sys.stderr, level=logging.INFO)
+    logging.getLogger("urllib3").setLevel(logging.INFO)
+
+    mqttc = paho.Client('netatmo-mqtt-connector', clean_session=True)
+    # mqttc.enable_logger()
+    mqttc.will_set("{}/connectorstatus".format(NETATMO_MQTT_PREFIX), "netatmo Connector: LOST_CONNECTION", 0, retain=True)
+
+    mqttc.connect(BROKER_HOST, BROKER_PORT, 60)
+    logging.info("Connected to {}:{}".format(BROKER_HOST, BROKER_PORT))
+
+    mqttc.publish("{}/connectorstatus".format(NETATMO_MQTT_PREFIX), "netatmo Connector: ON-LINE", retain=True)
+
+    mqttc.loop_start()
+    while True:
+        try:
+            values = get_values()
+            for k, v in values.items():
+                (result, mid) = mqttc.publish("{}/{}".format(NETATMO_MQTT_PREFIX, k), str(v), 0)
+                logging.debug("Pubish Result: {} MID: {} for {}: {}".format(result, mid, k, v))  # noqa E501                
+
+            time.sleep(FREQUENCY)
+        except KeyboardInterrupt:
+            break
+        except Exception:
+            raise
+
+    mqttc.publish("{}/connectorstatus".format(NETATMO_MQTT_PREFIX), "netatmo Connector: OFF-LINE", retain=True)
+
+    mqttc.disconnect()
+    mqttc.loop_stop()  # waits, until DISCONNECT message is sent out
+    logging.info("Disconnected from to {}:{}".format(BROKER_HOST, BROKER_PORT))
