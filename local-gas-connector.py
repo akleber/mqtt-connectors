948efa150efa18fdf6807cec66c267e2c81024c5
Andreas Kleber <andreas@drosselweg7a.de>
2020-05-17 16:45:26 +0200

    add local-gas-connector

---
+++ local-gas-connector.py
@@ -0,0 +1,94 @@
+#!/usr/bin/env python
+
+import paho.mqtt.client as paho  # pip install paho-mqtt
+import time
+import logging
+import sys
+import pigpio
+
+MQTT_PREFIX = 'gas'
+BROKER_HOST = 'rpi3.kleber'
+BROKER_PORT = 1883
+FREQUENCY_S = 2
+GAS_GPIO = 27
+
+m3abs = 0.0
+gpio = None
+reed_state_old = 1
+
+
+def read_state():
+    global m3abs
+
+    with open('local-gas-connector.state') as f:
+        m3abs = float(f.readline().strip())
+
+    logging.info("Read initial value: {.3}".format(m3abs))
+
+
+def init_gpio():
+    global gpio
+    gpio = pigpio.pi()
+    gpio.set_mode(GAS_GPIO, pigpio.INPUT)
+    gpio.set_pull_up_down(GAS_GPIO, pigpio.PUD_UP)
+
+    logging.info('GPIO initialized')
+
+
+def data():
+    global reed_state_old
+    global m3abs
+
+    values = {}
+    reed_state = gpio.read(GAS_GPIO)
+
+    if reed_state == 1:
+        logging.info("Reed state open")
+        reed_state_old = reed_state
+    else:
+        logging.info("Reed state closed")
+
+        if reed_state_old != reed_state:
+            reed_state_old = reed_state
+
+            m3abs += 0.001
+            values['gas/volume'] = str(m3abs)
+            values['gas/tick'] = '1'
+
+    return values
+
+
+if __name__ == '__main__':
+    logging.basicConfig(stream=sys.stderr, level=logging.INFO)
+
+    read_state()
+    init_gpio()
+
+    mqttc = paho.Client('local-gas-connector', clean_session=True)
+    # mqttc.enable_logger()
+    mqttc.will_set("{}/connectorstatus".format(MQTT_PREFIX), "Local Gas Connector: LOST_CONNECTION", 0, retain=True)
+
+    mqttc.connect(BROKER_HOST, BROKER_PORT, 60)
+    logging.info("Connected to {}:{}".format(BROKER_HOST, BROKER_PORT))
+
+    mqttc.publish("{}/connectorstatus".format(MQTT_PREFIX), "Local Gas Connector: ON-LINE", retain=True)
+
+    mqttc.loop_start()
+    while True:
+        try:
+            values = data()
+            for k, v in values.items():
+                (result, mid) = mqttc.publish("{}/{}".format(MQTT_PREFIX, k), str(v), 0)
+                logging.debug("Pubish Result: {} MID: {} for {}: {}".format(result, mid, k, v))  # noqa E501                
+
+            time.sleep(FREQUENCY_S)
+        except KeyboardInterrupt:
+            break
+        except Exception:
+            raise
+
+    mqttc.publish("{}/connectorstatus".format(MQTT_PREFIX), "Local Gas Connector: OFF-LINE", retain=True)
+
+    mqttc.disconnect()
+    mqttc.loop_stop()  # waits, until DISCONNECT message is sent out
+    logging.info("Disconnected from to {}:{}".format(BROKER_HOST, BROKER_PORT))
