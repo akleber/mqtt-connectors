d341c0b17f94a422c2efc2c2ef1a4ea2c23f608e
root <root@mosquitto.kleber>
2024-08-11 08:54:28 +0000

    updated hostnames

--- config.py
+++ config.py
@@ -1,15 +1,15 @@
 #!/usr/bin/env python
 
-BROKER_HOST = 'mosquitto.kleber'
+BROKER_HOST = 'mosquitto.kleber.home.arpa'
 BROKER_PORT = 1883
 
-INFLUXDB_HOST = 'influxdb.kleber'
+INFLUXDB_HOST = 'influxdb.kleber.home.arpa'
 
-FRONIUS_HOST = 'fronius.kleber'
+FRONIUS_HOST = 'fronius.kleber.home.arpa'
 FRONIUS_MQTT_PREFIX = 'fronius'
 ENERGYMETER_MQTT_PREFIX = 'energymeter'
 
-MODBUS_HOST = 'fronius.kleber'
+MODBUS_HOST = 'fronius.kleber.home.arpa'
 BATTERY_MQTT_PREFIX = 'battery'
 
 PV_P_TOPIC = 'fronius/p_pv'
@@ -18,7 +18,7 @@ CHG_PCT_TOPIC = 'battery/chg_pct'
 AUTO_CHG_TOPIC = 'battery/auto_chg_pct'
 SOC_TOPIC = 'fronius/soc'
 
-GOECHARGER_HOST = 'go-echarger.kleber'
+GOECHARGER_HOST = 'go-echarger.kleber.home.arpa'
 GOECHARGER_MQTT_PREFIX = 'goe'
 
 NETATMO_MQTT_PREFIX = 'netatmo'
